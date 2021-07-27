584e6332b4984db3a7d8d7ba7f11f22a194dfe79
srpg <44681214+srpg@users.noreply.github.com>
2021-07-27 17:38:27 +0300

    Fixed suicide sound work after recent commit

--- quakesound/addons/source-python/plugins/quakesound/quakesound.py
+++ quakesound/addons/source-python/plugins/quakesound/quakesound.py
@@ -53,6 +53,10 @@ def player_death(args):
 	userid = args.get_int('userid')
 	attacker = args.get_int('attacker')
 	if attacker > 0:
+		if userid == attacker: # Suicide
+			players[userid] = 0 # Not sure is this require to "start over" from kills
+			for i in soundlib.getUseridList():
+				soundlib.playgamesound(i, 'quake/suicide.wav')
 		if not soundlib.getTeam(userid) == soundlib.getTeam(attacker):
 			players[userid] = 0
 			players[attacker] += 1
@@ -75,11 +79,7 @@ def player_death(args):
 			if args.get_string('weapon') == 'knife':
 				for i in soundlib.getUseridList():
 					soundlib.playgamesound(i, 'quake/humiliation.mp3')
-	else: # Not sure will this method count "Suicide"
-		players[userid] = 0
-		for i in soundlib.getUseridList():
-			soundlib.playgamesound(i, 'quake/suicide.wav')
-                
+
 def setFirstblood(a):
 	global _firstblood
 	_firstblood = a
